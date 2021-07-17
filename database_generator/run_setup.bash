a20db580ddbeec1881487de93744da79a240cae3
chris1nexus <chris.2nexus@gmail.com>
2021-07-17 15:30:41 +0200

    changed setup.bash

--- database_generator/run_setup.bash
+++ database_generator/run_setup.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
+cd ./eurostat_lib/
 bash ./eurostat_lib/run_eurostat_downloader.bash
+cd ../policies_lib/
 bash ./policies_lib/run_policies_downloader.bash
+cd ../db_gen_lib/
 bash ./db_gen_lib/run_db_generator.bash
+cd ../
